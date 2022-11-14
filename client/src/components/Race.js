eae2aed9208f9f0d26e56bc3f9b43935344ea9b6
Logan Lampton <lampton.logan@gmail.com>
2022-11-14 11:34:50 -0500

    more work on Race component/creating popups

--- client/src/components/Race.js
+++ client/src/components/Race.js
@@ -1,10 +1,26 @@
 import React, { useState } from 'react';
 
 import DragonBorn from './RaceComponents/DragonBorn';
+import Dwarf from './RaceComponents/Dwarf';
+import Elf from './RaceComponents/Elf';
+import HalfElf from './RaceComponents/HalfElf';
+import Gnome from './RaceComponents/Gnome';
+import HalfOrc from './RaceComponents/HalfOrc';
+import Halfling from './RaceComponents/Halfling';
+import Human from './RaceComponents/Human';
+import Tiefling from './RaceComponents/Tiefling';
 
 export default function Race() {
     
-    const [buttonPopup, setButtonPopup] = useState(false);
+    const [dragonbornPopup, setDragonbornPopup] = useState(false);
+    const [dwarfPopup, setDwarfPopup] = useState(false);
+    const [elfPopup, setElfPopup] = useState(false);
+    const [gnomePopup, setGnomePopup] = useState(false);
+    const [halfElfPopup, setHalfElfPopup] = useState(false);
+    const [halfOrcPopup, setHalfOrcPopup] = useState(false);
+    const [halflingPopup, setHalflingPopup] = useState(false);
+    const [humanPopup, setHumanPopup] = useState(false);
+    const [tieflingPopup, setTieflingPopup] = useState(false);
 
     function handleAddRace(event){
         event.preventDefault()
@@ -27,16 +43,24 @@ export default function Race() {
             <div className='buttonList'>
                 <h1>Race</h1>
                 <p>Choose your race below:</p>
-                <button onClick={() => setButtonPopup(true)}>Dragonborn</button>
-                    <DragonBorn trigger={buttonPopup} setTrigger={setButtonPopup}></DragonBorn>
-                <button>Dwarf</button>
-                <button>Elf</button>
-                <button>Gnome</button>
-                <button>Half-Elf</button>
-                <button>Half-Orc</button>
-                <button>Halfling</button>
-                <button>Human</button>
-                <button>Tiefling</button>
+                <button onClick={() => setDragonbornPopup(true)}>Dragonborn</button>
+                    <DragonBorn trigger={dragonbornPopup} setTrigger={setDragonbornPopup}></DragonBorn>
+                <button onClick={() => setDwarfPopup(true)}>Dwarf</button>
+                    <Dwarf trigger={dwarfPopup} setTrigger={setDwarfPopup}></Dwarf>
+                <button onClick={() => setElfPopup(true)}>Elf</button>
+                    <Elf trigger={elfPopup} setTrigger={setElfPopup}></Elf>
+                <button onClick={() => setGnomePopup(true)}>Gnome</button>
+                    <Gnome trigger={gnomePopup} setTrigger={setGnomePopup}></Gnome>
+                <button onClick={() => setHalfElfPopup(true)}>Half-Elf</button>
+                    <HalfElf trigger={halfElfPopup} setTrigger={setHalfElfPopup}></HalfElf>
+                <button onClick={() => setHalfOrcPopup(true)}>Half-Orc</button>
+                    <HalfOrc trigger={halfOrcPopup} setTrigger={setHalfOrcPopup}></HalfOrc>
+                <button onClick={() => setHalflingPopup(true)}>Halfling</button>
+                    <Halfling trigger={halflingPopup} setTrigger={setHalflingPopup}></Halfling>
+                <button onClick={() => setHumanPopup(true)}>Human</button>
+                    <Human trigger={humanPopup} setTrigger={setHumanPopup}></Human>
+                <button onClick={() => setTieflingPopup(true)}>Tiefling</button>
+                    <Tiefling trigger={tieflingPopup} setTrigger={setTieflingPopup}></Tiefling>
             </div> 
         </>
     );
